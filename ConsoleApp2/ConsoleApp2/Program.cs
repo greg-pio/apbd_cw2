8250349f28bb6580818db8889d52e22c31ba1da2
unknown <grz.piotr@wp.pl>
2020-03-22 06:24:01 -0700

    Obsluga wyjatkow

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,6 +13,16 @@ namespace ConsoleApp2
             string csvpath;
             string xmlpath;
             string filetype;
+            string logpath = @"C:\Users\ger\Desktop\log.txt";
+            DateTime localDate = DateTime.Now;
+
+            if (!File.Exists(logpath))
+            {
+                using (StreamWriter sw = File.CreateText(logpath))
+                {
+                    sw.WriteLine("New log file created " + localDate);
+                }
+            }
 
             if (args.Length < 3)
             {
@@ -24,32 +34,50 @@ namespace ConsoleApp2
                 csvpath = args[0];
                 xmlpath = args[1];
                 filetype = args[2];
-            } 
-                                    
-            var fi = new FileInfo(csvpath);
-            using (var stream = new StreamReader(fi.OpenRead()))
+            }
 
+            try
             {
-                var list = new List<Student>();
-                string line = null;
-                while ((line = stream.ReadLine()) != null)
+                var fi = new FileInfo(csvpath);
+                using (var stream = new StreamReader(fi.OpenRead()))
                 {
-                    string[] kolumny = line.Split(',');
-                    var st = new Student
+                    var list = new List<Student>();
+                    string line = null;
+                    while ((line = stream.ReadLine()) != null)
                     {
-                        Imie = kolumny[0],
-                        Nazwisko = kolumny[1]
-                    }; 
-                    list.Add(st);
-                }
-                stream.Dispose();
+                        string[] kolumny = line.Split(',');
+                        var st = new Student
+                        {
+                            Imie = kolumny[0],
+                            Nazwisko = kolumny[1]
+                        };
+                        list.Add(st);
+                    }
+                    stream.Dispose();
 
-                FileStream writer = new FileStream(xmlpath, FileMode.Create);
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
-                                           new XmlRootAttribute("uczelnia"));
-                serializer.Serialize(writer, list);
-                serializer.Serialize(writer, list);
-            }          
+                    FileStream writer = new FileStream(xmlpath, FileMode.Create);
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                                               new XmlRootAttribute("uczelnia"));
+                    serializer.Serialize(writer, list);
+                    serializer.Serialize(writer, list);
+                }
+            } catch (ArgumentException e)
+            {
+                Console.WriteLine("Podana sciezka jest niepoprawna.");
+                using (StreamWriter sw = File.AppendText(logpath))
+                {
+                    sw.WriteLine("Podana sciezka jest niepoprawna.");
+                    sw.WriteLine(e);
+                }
+            } catch (FileNotFoundException f)
+            {
+                Console.WriteLine("Plik nie istnieje");
+                using (StreamWriter sw = File.AppendText(logpath))
+                {
+                    sw.WriteLine("Plik nie istnieje");
+                    sw.WriteLine(f);
+                }
+            }
         }
     }
 }
